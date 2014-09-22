9533db04498e0e8e5c68ce9e5bcf8b73bf3551f0
SKSC <sha@sksc.me>
2014-09-22 15:27:46 +1000

    Python study

---
+++ EOIApplicationAutoFiller/src/ApplicationFiller.py
@@ -0,0 +1,260 @@
+from selenium import webdriver;
+
+registerurl="https://skillselect.gov.au/SKILLSELECT/ExpressionOfInterest/PreReg/Start"
+
+def process():
+    #initialize
+    dr = webdriver.Firefox()
+    
+    #page 1 -- terms and conditions
+    dr.find_element_by_xpath("//input[@type='checkbox']").click() #tick on the checkbox
+    dr.find_element_by_xpath("//button[@name='Next']").click()  # click on next button
+    
+    #page 2 -- personal details
+    fn = dr.find_element_by_xpath("//input[@id='EOI_PersonalDetails_FamilyName']")
+    fn.send_keys('FamilyName') #find and set family name
+    fn = dr.find_element_by_xpath("//input[@id='EOI_PersonalDetails_GivenNames']")
+    fn.send_keys('Given Names') #find and set family name
+    dr.find_element_by_xpath("//input[@name='EOI.PersonalDetails.Sex' and @value='M']").click() #select the sex
+    dob = dr.find_element_by_xpath("//input[@id='EOI_PersonalDetails_DateOfBirth']")
+    dob.send_keys("01/01/1988") #set date of birth
+    cob = dr.find_element_by_xpath("//select[@id='EOI_PersonalDetails_CountryOfPassport']")
+    cob.send_keys("CHINA") #set country of birth
+    cop = dr.find_element_by_xpath("//select[@id='EOI_PersonalDetails_CountryOfPassport']")
+    cop.send_keys("CHINA") #set country of birth
+    #set the citizenship
+    dr.find_element_by_xpath("//input[@name='EOI.PersonalDetails.IsCitizenOfPassportCountry' and @value='True']").click()
+    #set the citizenship of another country
+    dr.find_element_by_xpath("//input[@name='EOI.PersonalDetails.IsCitizenOfAnotherCountry' and @value='False']").click()
+    #other passport
+    dr.find_element_by_xpath("//input[@name='EOI.PersonalDetails.HasOtherPassports' and @value='False']").click()
+    #usual country of residence
+    ucr = dr.find_element_by_xpath("//select[@id='EOI_PersonalDetails_CountryOfResidence']")
+    ucr.send_keys("AUSTRALIA")
+    state = dr.find_element_by_xpath("//select[@id='EOI_PersonalDetails_State']")
+    state.send_keys("Victoria")
+    dr.find_element_by_xpath("//input[@id='EOI_PersonalDetails_Postcode']").send_keys('3163')
+    #relationship status
+    rs = dr.find_element_by_xpath("//select[@id='EOI_PersonalDetails_RelationshipStatus']")
+    rs.send_keys('NEVER MARRIED')
+    #press button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 3 -- confirm provided information
+    #confirm information correct
+    dr.find_element_by_xpath("//input[@name='EOI.PersonalDetails.IdentityConfirmed' and @value='True']").click()
+    #click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 4 -- create account
+    dr.find_element_by_xpath("//input[@id='UserVM_Password']").send_keys('Asdf11111') #set passport
+    dr.find_element_by_xpath("//input[@id='UserVM_PasswordConfirmation']").send_keys('Asdf11111') #confirm passport
+    dr.find_element_by_xpath("//input[@id='UserVM_Email']").send_keys('test@sksc.me') # set email
+    dr.find_element_by_xpath("//input[@id='UserVM_EmailConfirmation']").send_keys('test@sksc.me') #confirm email
+    #set preferred passport
+    dr.find_element_by_xpath("//select[@id='UserVM_SecretAnswer1']").send_keys('Preferred Passport')
+    #passport number
+    dr.find_element_by_xpath("//input[@id='UserVM_SecretAnswer2']").send_keys('G30912345')
+    #expire date
+    dr.find_element_by_xpath("//input[@id='UserVM_SecretAnswer3']").send_keys('06/08/2024')
+    #
+    #Google chapture, do know how to do yet
+    #
+    #
+    #click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 5 -- confirm account information
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 6 -- skillselect login
+    #confirm the policy
+    dr.find_element_by_xpath("//input[@id='chkIAccept']").click()
+    #user name
+    dr.find_element_by_xpath("//input[@name='txtUsername']").send_keys("E0004071191")
+    #passport
+    dr.find_element_by_xpath("//input[@name='txtPassword']").send_keys("Asdf11111")
+    #click login button
+    dr.find_element_by_xpath("//input[@name='Button1']").click()
+    
+    
+    #page 7 -- EOI(3/12) -- Visa Type
+    #visa type
+    dr.find_element_by_xpath("//input[@name='VisaSubclassVM.HasSkilledIndependentVisa189']").click()
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 8 -- EOI(4/12) --Family Member
+    #include family member or not
+    dr.find_element_by_xpath("//input[@name='EOI.FamilyMembers.HasFamilyMembers' and @value='False']").click()
+    #will include family member in the future or not
+    dr.find_element_by_xpath("//input[@name='EOI.FamilyMembers.IsAccompaniedByPartner' and @value='False']").click()
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 9 EOI(5/12) -- English Language
+    #undertake IELTE
+    dr.find_element_by_xpath("//input[@name='EOI.ClientLanguage.HasUndertakenEnglishTest' and @value='True']").click()
+    #IELTS
+    dr.find_element_by_xpath("//input[@name='EOI.ClientLanguage.TestName' and @value='IELTS']").click()
+    #IELTS Date
+    dr.find_element_by_xpath("//input[@name='EOI.ClientLanguage.TestDate']").send_keys("09/09/2013")
+    #IELTS Reference number
+    dr.find_element_by_xpath("//input[@name='EOI.ClientLanguage.TestReferenceNumber']").send_keys("11AU305774FAMG240G")
+    #listening score
+    dr.find_element_by_xpath("//select[@name='EOI.ClientLanguage.ListeningScoreIELTS']").send_keys("7")
+    #reading score
+    dr.find_element_by_xpath("//select[@name='EOI.ClientLanguage.ReadingScoreIELTS']").send_keys("7")
+    #Writing score
+    dr.find_element_by_xpath("//select[@name='EOI.ClientLanguage.WrittenScoreIELTS']").send_keys("7")
+    #Speaking score
+    dr.find_element_by_xpath("//select[@name='EOI.ClientLanguage.SpeakingScoreIELTS']").send_keys("7")
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 10 EOI(6/12) -- Education
+    #undertake IELTE
+    dr.find_element_by_xpath("//input[@name='EOI.ClientEducation.HasPreviouslyStudied' and @value='True']").click()
+    #Can add multiple education history
+    #Add education history
+    dr.find_element_by_xpath("//button[@name='AddEducationHistory']").click()
+    #Qualification
+    dr.find_element_by_xpath("//select[@name='EducationVM.AddEducationHistory.Qualification']").send_keys("Masters Degree in Science, Business or Technology")
+    #course name
+    dr.find_element_by_xpath("//input[@name='EducationVM.AddEducationHistory.CourseName']").send_keys("Master of Computer Science")
+    #institution name
+    dr.find_element_by_xpath("//input[@name='EducationVM.AddEducationHistory.InstitutionName']").send_keys("RMIT")
+    #Country of Institution
+    dr.find_element_by_xpath("//select[@name='EducationVM.AddEducationHistory.Country']").send_keys("Australia")
+    #Institution campus
+    dr.find_element_by_xpath("//input[@name='EducationVM.AddEducationHistory.Campus']").send_keys("City")
+    #postcode of campus
+    dr.find_element_by_xpath("//input[@name='EducationVM.AddEducationHistory.CampusPostCode']").send_keys("3000")
+    #date from
+    dr.find_element_by_xpath("//input[@name='EducationVM.AddEducationHistory.DateFrom']").send_keys("15/03/2011")
+    #date to
+    dr.find_element_by_xpath("//input[@name='EducationVM.AddEducationHistory.DateTo']").send_keys("15/03/2014")
+    #confirm
+    dr.find_element_by_xpath("//button[@name='InsertEducationHistory']").click()
+    #
+    #Can add multiple education history
+    #Australian study requirement
+    dr.find_element_by_xpath("//input[@name='EOI.ClientEducation.MeetStudyRequirements' and @value='True']").click()
+    #regional Australia -- study
+    dr.find_element_by_xpath("//input[@name='EOI.ClientEducation.HasStudiedRegional' and @value='True']").click()
+    # Credentialled community language
+    dr.find_element_by_xpath("//input[@name='EOI.ClientEducation.HasCommunityLanguageQualification' and @value='True']").click()
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    
+    #page 11 EOI(7/12) -- Skills assessment
+    #nominated occupation
+    dr.find_element_by_xpath("//input[@name='EOI.ClientSkills.NominatedOccupation.ANZSCO']").send_keys("Developer Programmer - 261312")
+    # has assessment from relevant authority
+    dr.find_element_by_xpath("//input[@name='EOI.ClientSkills.HasSkillsAssessment' and @value='True']").click()
+    # select the assessing authority
+    dr.find_element_by_xpath("//select[@name='EOI.ClientSkills.AssessmentAuthority']").send_keys("Australian Computer Society")
+    #assessment date
+    dr.find_element_by_xpath("//input[@name='EOI.ClientSkills.AssessmentDate']").send_keys("05/06/2014")
+    #assessment reference number
+    dr.find_element_by_xpath("//input[@name='EOI.ClientSkills.ReferenceNumber']").send_keys("GXXXXXXXXX")
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    #page 12 EOI(8/12) -- Employment and Professional Year
+    #Can add multiple education history
+    #
+    #
+    #
+    #Add employment history
+    dr.find_element_by_xpath("//button[@name='AddEmploymentHistory']").click()
+    #employment position
+    ep = dr.find_element_by_xpath("//input[@name='EmploymentVM.AddEmploymentHistory.Position']")
+    ep.send_keys("Development Manger")
+    #employer name
+    en = dr.find_element_by_xpath("//input[@name='EmploymentVM.AddEmploymentHistory.EmployerName']")
+    en.send_keys("IDTC")
+    #employer country
+    ec = dr.find_element_by_xpath("//select[@name='EmploymentVM.AddEmploymentHistory.Country']")
+    ec.send_keys("AUSTRALIA")
+    #employer state
+    es = dr.find_element_by_xpath("//select[@name='EmploymentVM.AddEmploymentHistory.State']")
+    es.send_keys("Victoria")
+    #related nominated occupation
+    dr.find_element_by_xpath("//input[@name='EmploymentVM.AddEmploymentHistory.IsRelatedNominatedOccupation' and @value='True']").click()
+    #starting date
+    sd = dr.find_element_by_xpath("//input[@name='EmploymentVM.AddEmploymentHistory.DateFrom']")
+    sd.send_keys("02/02/2011")
+    #ending date
+    ed = dr.find_element_by_xpath("//input[@name='EmploymentVM.AddEmploymentHistory.DateTo']")
+    ed.send_keys("18/09/2014")
+    #confirm 
+    ed = dr.find_element_by_xpath("//button[@name='InsertEmploymentHistory']").click()
+    #
+    #
+    #
+    #professional year
+    dr.find_element_by_xpath("//input[@name='EOI.ClientEmployment.HasCompletedProfessionYear' and @value='True']").click()
+    #professional year field
+    pyf = dr.find_element_by_xpath("//select[@name='EOI.ClientEmployment.CompletedProfessionalYear']")
+    pyf.send_keys("Computer Science")
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    
+    
+    #page 13 EOI(9/12) Confirmation of Points
+    #language
+    dr.find_element_by_xpath("//input[@name='EOI.PointsConfirmation.LanguageAbilityConfirmed' and @value='True']").click()
+    #education
+    dr.find_element_by_xpath("//input[@name='EOI.PointsConfirmation.EducationHistoryConfirmed' and @value='True']").click()
+    #study requirement
+    dr.find_element_by_xpath("//input[@name='EOI.PointsConfirmation.MeetStudyRequirementConfirmed' and @value='True']").click()
+    #regional Australia Study
+    dr.find_element_by_xpath("//input[@name='EOI.PointsConfirmation.RegionalStudyConfirmed' and @value='True']").click()
+    #Credentialled community language
+    dr.find_element_by_xpath("//input[@name='EOI.PointsConfirmation.CommunityLanguageConfirmed' and @value='True']").click()
+    #Employment
+    dr.find_element_by_xpath("//input[@name='EOI.PointsConfirmation.EmploymentHistoryConfirmed' and @value='True']").click()
+    #Professional Year
+    dr.find_element_by_xpath("//input[@name='EOI.PointsConfirmation.ProfessionalYearConfirmed' and @value='True']").click()
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    
+    
+    #page 14 EOI(10/12) Declarations
+    #read and understand
+    dr.find_element_by_xpath("//input[@name='EOI.Declarations.HasReadAndUnderstood' and @value='True']").click()
+    #complete correct
+    dr.find_element_by_xpath("//input[@name='EOI.Declarations.HasCompleteCorrect' and @value='True']").click()
+    #understand misleading
+    dr.find_element_by_xpath("//input[@name='EOI.Declarations.HasUnderstoodMisleading' and @value='True']").click()
+    #not visa
+    dr.find_element_by_xpath("//input[@name='EOI.Declarations.HasUnderstoodNotVisa' and @value='True']").click()
+    #update changes
+    dr.find_element_by_xpath("//input[@name='EOI.Declarations.WillUpdateChanges' and @value='True']").click()
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    
+    #page 15 EOI(11/12) Review Page
+    #review confirm
+    dr.find_element_by_xpath("//input[@name='EOI.ReviewConfirm' and @value='True']").click()
+    #confirm and click Next button
+    dr.find_element_by_xpath("//button[@name='Next']").click()
+    
+    
+    #page 16 EOI(12/12) Visa type summary
+    dr.find_element_by_xpath("//button[@name='Submit']").click()
+    
+def main():
+    process()
+    
+    
+main()
+    
+    
+    
---
+++ WebCrawlerForDynamicSite/src/crawler.py
@@ -0,0 +1,263 @@
+import urllib2
+import httplib
+import mysql.connector
+from mysql.connector import errorcode
+import re
+import types
+import time
+import sys
+import os.path
+from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool as ThreadPool
+
+url="http://www.sassandbide.com/sitemap.xml"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.114 Safari/537.36"
+
+def insertDB(item):
+    config = {
+              'user': 'root',
+              'password':'',
+              'database':'sass'
+              }
+    try:
+        cnx = mysql.connector.connect(**config)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print "Something is wrong with your login"
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print "Database does not exists"
+        else:
+            print (err)
+    cursor = cnx.cursor()
+    add_item =("INSERT INTO itemInfo " 
+               "(Name, Des, Detail, Color, Info, imageBox, Image, Size, Prize, URL) "
+               "VALUES (%(name)s, %(desc)s, %(detail)s, %(color)s, %(info)s, %(imageBox)s, %(image)s, %(size)s, %(prize)s, %(url)s)")
+    try:
+        data_item = {
+                     "name"      : str(item['title']),
+                     "desc"      : str(item['desc']),
+                     "detail"    : str(item['detail']),
+                     "color"     : str(item['color']),
+                     "info"      : str(item['info']),
+                     "imageBox"  : str(item['imageBox']),
+                     "image"     : str(item['image']),
+                     "size"      : str(item['size']),
+                     "prize"     : str(item['prize']),
+                     "url"       : str(item['url']),
+                     }
+    except UnicodeEncodeError:
+        print str(item['title'])
+    else:
+        try:
+            cursor.execute(add_item, data_item)
+            cnx.commit()
+            cursor.close()
+            cnx.close()
+        except mysql.connector.errors.DataError:
+            print str(item['title'])
+    
+def polishAtt(name, detail, color,info,imageBox, image,size, prize,url):
+    item = {
+            'title'  : '',
+            'desc'   : '',
+            'detail' : detail,
+            'color'  : color,
+            'info'   : '',
+            'prize'  : prize,
+            'imageBox' : imageBox,
+            'image'  :'',
+            'size'   :'',
+            'url'    :url,
+            }
+    if type(name) is types.StringType:
+        item['title'] = name
+    else:
+        item['title'] = name['title']
+        item['desc']  = name ['desc']
+        
+    if type(info) is types.StringType:
+        item['info']=info
+    else:
+        for i in range(0,len(info)):
+            item['info']+= "\n"+info[i]
+    if type(image) is types.StringType:
+        item['image']=image
+    else:
+        for i in range(0,len(image)):
+            item['image']+="\n"+image[i]
+    if type(size) is types.StringType:
+        item['size']=size
+    else:
+        for i in range (0,len(size)):
+            item['size']+="\n"+size[i]
+    return item
+
+def fetch(url):
+    try:
+        req = urllib2.Request(url)
+        response = urllib2.urlopen(req)
+        content = response.read()
+    except urllib2.HTTPError,httplib.BadStatusLine:
+        print url
+        return False
+    else:
+        return content
+
+def parserSitemap(sitemap):
+    m=re.compile("http://backoffice\S*?html");
+    clothSite=m.findall(sitemap)
+    return clothSite
+    
+def parserName(content):
+    soup = BeautifulSoup(content)
+    itemName = soup.find_all('div', class_='product-name')
+    try:
+        soup = BeautifulSoup(str(itemName[0]))
+    except IndexError:
+        return False
+    else:
+        itemName = {
+                    'title' : soup.h1.string,
+                    'desc' : soup.p.string,
+                    }
+        return itemName
+    
+def parserDetail(content):
+    soup = BeautifulSoup(content)
+    itemDetail = soup.find_all('div', class_='box-collateral box-description')
+    try:
+        soup = BeautifulSoup(str(itemDetail[0]))
+        itemDetail = soup.find_all('div')
+        soup = BeautifulSoup(str(itemDetail[1]))
+    except IndexError:
+        return "Detail does not exist"
+    else:
+        itemDetail = soup.div.get_text()
+        return itemDetail.strip()
+    
+def parserColour(content):
+    soup = BeautifulSoup(content)
+    itemColor = soup.select('fieldset[class="product-options"]')
+    try:
+        soup = BeautifulSoup(str(itemColor[0]))
+        itemColor = soup.select('option[selected="selected"]')
+        soup = BeautifulSoup(str(itemColor[0]))
+        itemColor = soup.option.get_text()
+    except IndexError:
+        return "color not exists"
+    else:
+        return itemColor
+
+def parserInfo(content):
+    soup = BeautifulSoup(content)
+    try:
+        itemInfo = soup.find_all("div",id="tab-1")
+        soup = BeautifulSoup(str(itemInfo[0]))
+    except IndexError:
+        return "Info does not exist"
+    else:
+        itemInfo = (soup.find_all('p',limit = 2))
+        info = []
+        for i in range(0,len(itemInfo)):
+            soup = BeautifulSoup(str(itemInfo[i]))
+            info.append(soup.p.get_text())
+        return info
+    
+def parserImage(content):
+    soup = BeautifulSoup(content)
+    itemImage = soup.find_all("ul",class_="overview")
+    try:
+        soup = BeautifulSoup(str(itemImage[0]))
+    except IndexError:
+        return "Image does not exist"
+    else:
+        itemImage = soup.select("a[href]")
+        imageurl=[]
+        for item in itemImage:
+            soup = BeautifulSoup(str(item))
+            imageurl.append(soup.a['href'])
+        itemImage = downloadImage(imageurl)
+        return itemImage 
+
+def parserImageContainer(content):
+    soup = BeautifulSoup(content)
+    itemImage = soup.find_all("a",class_="MagicZoom")
+    imageurl = []
+    try:
+        imageurl.append(str(itemImage[0]['href']))
+    except IndexError:
+        return "Image does not exist"
+    else:
+        itemImage = downloadImage(imageurl)
+        return itemImage 
+    
+def parserSize(content):
+    size = re.compile('"label":[\S\s]*?,')
+    #the size in first place is not the correct one
+    itemSize = size.findall(content)
+    size= []
+    for i in range(1,len(itemSize)):
+        size.append(itemSize[i][9:len(itemSize[i])-2])
+    if len(size) <= 1:
+        return "Size does not exist"
+    else:
+        return size
+
+def parserPrice(content):
+    soup = BeautifulSoup(content)
+    itemPrice = soup.find_all("span",class_="price special-price")
+    if itemPrice ==[]:
+        itemPrice = soup.find_all("span",class_="price")
+    try:
+        soup = BeautifulSoup(str(itemPrice[0]))
+    except IndexError:
+        return False
+    else:
+        itemPrice = soup.span.get_text().strip()
+        return itemPrice
+    
+def downloadImage(imageurl):
+    imagePath=[]
+    for url in imageurl:
+        filename = os.path.basename(url)
+        socket = urllib2.urlopen(urllib2.Request(url))
+        data = socket.read()
+        socket.close()
+        file = open('/Users/SHA/Desktop/bluehost/sb/'+filename, 'w')
+        file.write(data)
+        file.flush()
+        file.close()
+        imagePath.append(filename)
+    return imagePath
+    
+def runThread(url):
+    content = fetch(url)
+    if content !=False:
+        name    = parserName(content)
+        if name !=False:
+            detail  = parserDetail(content)
+            color   = parserColour(content)
+            info    = parserInfo(content)
+            imageBox = parserImageContainer(content)
+            image   = parserImage(content)
+            size    = parserSize(content)
+            prize   = parserPrice(content)
+            item = polishAtt(name, detail, color,info,imageBox, image,size, prize,url)
+            insertDB(item)
+        
+def main():
+    start = time.time()
+    reload(sys)
+#    sys.setdefaultencoding('utf-8')
+    sitemap = fetch(url)
+    clothSite = parserSitemap(sitemap)
+    print "total "+str(len(clothSite))
+    pool = ThreadPool(8)
+    pool.map(runThread, clothSite)
+    pool.close() 
+    pool.join() 
+    print "done"
+    print str(time.time()-start)
+
+    
+main()
